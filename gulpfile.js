1541c9bce666b9b473e9985146ea963df55d8ba6
baiyangliu <cun.liu@qq.com>
2017-01-11 17:34:04 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,27 @@ var minifycss = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
-// 压缩css文件
-gulp.task('minify-css', function() {
-  return gulp.src('./public/**/*.css')
-  .pipe(minifycss())
-  .pipe(gulp.dest('./public'));
+var imagemin = require('gulp-imagemin');
+
+gulp.task('minify-css', function () {
+    return gulp.src('./public/**/*.css').pipe(minifycss()).pipe(gulp.dest('./public'));
 });
-// 压缩html文件
-gulp.task('minify-html', function() {
-  return gulp.src('./public/**/*.html')
-  .pipe(htmlclean())
-  .pipe(htmlmin({
-    removeComments: true,
-    minifyJS: true,
-    minifyCSS: true,
-    minifyURLs: true,
-  }))
-  .pipe(gulp.dest('./public'))
+
+gulp.task('minify-html', function () {
+    return gulp.src('./public/**/*.html').pipe(htmlclean()).pipe(htmlmin({
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+    })).pipe(gulp.dest('./public'))
 });
-// 压缩js文件
-gulp.task('minify-js', function() {
-  return gulp.src('./public/**/*.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('./public'));
+
+gulp.task('minify-js', function () {
+    return gulp.src('./public/**/*.js').pipe(uglify()).pipe(gulp.dest('./public'));
 });
-// 默认任务
-gulp.task('default', [
-  'minify-html','minify-css','minify-js'
-]);
+
+gulp.task('minify-image', function () {
+    gulp.src('./public/**/*.{png,jpg,gif,ico,svg,jpeg}').pipe(imagemin()).pipe(gulp.dest('./public'));
+});
+
+gulp.task('default', ['minify-html', 'minify-css', 'minify-js', 'minify-image']);
