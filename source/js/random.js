68cb4f270f879383f11df517c76df7dcaefbb0ef
baiyangliu <cun.liu@qq.com>
2017-01-15 13:31:05 +0800

    updated

--- source/js/random.js
+++ source/js/random.js
@@ -1,30 +1,32 @@
-_g=function(){
-    var chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz";
-    var specal=false;
-    if($("#_specal_").is(":checked")){
-        chars+="~`!@#$%^&*()_+";
-        specal=true;
+_g = function() {
+    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz";
+    var specal = false;
+    if ($("#_specal_").is(":checked")) {
+        chars += "~`!@#$%^&*()_+";
+        specal = true;
     }
-    
-    var done=-1;
+
+    var done = -1;
     var str = "";
     var len = $("#_i_p_t").val();
-    var total = parseInt(len>>13);
-    var last = len%8192;
-    
-    
-    var work = function(len){
+    var total = parseInt(len >> 13);
+    var last = len % 8192;
+
+    var work = function(len) {
         var worker = new Worker("/js/random_worker.js");
-        worker.onmessage = function(evt){
+        worker.onmessage = function(evt) {
             str += evt.data;
             done++;
-            if(done==total){
+            if (done == total) {
                 $("#_result").html(str).show();
             }
         };
-        work.postMessage({specal:specal,len:len});
+        work.postMessage({
+            specal: specal,
+            len: len
+        });
     };
-    for(var i =0;i<=total;i++){
-        worker(i==total?8192:last);
+    for (var i = 0; i <= total; i++) {
+        worker(i == total ? 8192 : last);
     }
 };
