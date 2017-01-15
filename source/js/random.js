26489be90481c6cd1714bf55d669c52ae72199ca
baiyangliu <cun.liu@qq.com>
2017-01-15 14:16:22 +0800

    updated

--- source/js/random.js
+++ source/js/random.js
@@ -5,28 +5,22 @@ _g = function() {
         chars += "~`!@#$%^&*()_+";
         specal = true;
     }
-
-    var done = 0;
-    var str = "";
-    var len = $("#_i_p_t").val();
-    var total = 8;
-    var _len = len / total;
-    var last = len % total;
-
     var work = function(len) {
         var worker = new Worker("/js/random_worker.js");
         worker.onmessage = function(evt) {
             str += evt.data;
-            done++;
-            if (done == total) {
-                $("#_result").html(str).show();
-            }
+            $("#_result").html(str).show();
         };
         worker.postMessage({
             specal: specal,
             len: len
         });
     };
+    var str = "";
+    var len = $("#_i_p_t").val();
+    var total = 8;
+    var _len = len / total;
+    var last = len % total;
     for (var i = 0; i < total; i++) {
         work(i == total ? _len : _len + last);
     }
