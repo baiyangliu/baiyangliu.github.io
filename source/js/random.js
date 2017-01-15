a7940dc5daf7301508a3670d1ead94b2aa9b3659
baiyangliu <cun.liu@qq.com>
2017-01-15 14:46:22 +0800

    updated

--- source/js/random.js
+++ source/js/random.js
@@ -1,10 +1,5 @@
 _g = function() {
-    var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz";
-    var specal = false;
-    if ($("#_specal_").is(":checked")) {
-        chars += "~`!@#$%^&*()_+";
-        specal = true;
-    }
+    var specal = $("#_specal_").is(":checked");
     $("#_result").html("").hide();
     var work = function(len) {
         var worker = new Worker("/js/random_worker.js");
@@ -18,9 +13,12 @@ _g = function() {
     };
     var len = $("#_i_p_t").val();
     var total = 16;
-    var _len = len / total;
+    var _len = parseInt(len / total);
     var last = len % total;
     for (var i = 0; i < total; i++) {
-        work(i == total ? _len : _len + last);
+        var _l = i == total ? _len : _len + last;
+        if(_l!==0){
+            work(_l);
+        }
     }
 };
