7dda01e32d54ae5acb76c9e93a3e138867e3f26c
lc <lc@baiyangliu.com>
2017-12-30 22:15:15 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ let imagemin = require('gulp-imagemin');
 var pump = require('pump');
 
 gulp.task('minify-css', function () {
-    return gulp.src('./public/**/*.css').pipe(minifycss()).pipe(gulp.dest('./public'));
+    return gulp.src('./public/**/*.css').pipe(minifycss({compatibility: 'ie8'})).pipe(gulp.dest('./public'));
 });
 
 gulp.task('minify-html', function () {
@@ -29,4 +29,4 @@ gulp.task('minify-image', function () {
     return gulp.src('./public/**/*.{png,jpg,gif,ico,svg,jpeg}').pipe(imagemin()).pipe(gulp.dest('./public'));
 });
 
-gulp.task('default', ['minify-html', 'minify-css', 'minify-image']);
+gulp.task('default', ['minify-html', 'minify-image']);
