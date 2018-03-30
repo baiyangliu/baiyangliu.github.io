4a6d0a1529bedc5dcd844920b361c5a0556c6184
baiyangliu <cun.liu@qq.com>
2018-03-30 17:29:08 +0800

    Update one_key_install_strongswan_with_lets_encrypt_and_godaddy_on_centos7.sh

--- source/one_key_install_strongswan_with_lets_encrypt_and_godaddy_on_centos7.sh
+++ source/one_key_install_strongswan_with_lets_encrypt_and_godaddy_on_centos7.sh
@@ -162,9 +162,9 @@ firewall-cmd --reload
 
 echo configuring ip forward
 
-sysctl -w net.ipv4.ip_forward = 1
-sysctl -w net.ipv4.conf.all.accept_redirects = 0
-sysctl -w net.ipv4.conf.all.send_redirects = 0
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv4.conf.all.accept_redirects=0
+sysctl -w net.ipv4.conf.all.send_redirects=0
 
 
 sysctl -p
